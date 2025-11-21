8a35f25385ba8ecee56a6925e0b994ebdf92511a
starringtide <jayi0908@outlook.com>
2025-11-21 21:46:45 +0800

    CHORE: 添加网站基础架构

---
+++ docs/assets/javascripts/color.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function() { 
+    var p=localStorage.getItem("data-md-color-primary");
+    if (p){
+        document.body.setAttribute('data-md-color-primary',p);
+    }
+    var a=localStorage.getItem('data-md-color-accent');
+    if (a){
+        document.body.setAttribute('data-md-color-accent',a);
+    }
+    var s = localStorage.getItem('data-md-color-scheme');
+    if (s) {
+        document.body.setAttribute('data-md-color-scheme', s);
+    }
+
+}, false);
---
+++ docs/win+r+cli/cli.md
@@ -0,0 +1,3 @@
+# 真正的CS人，从命令行开始
+
+这部分的教程确实很多，比如前面提到的[实用技能拾遗](https://www.bilibili.com/video/BV1ry4y1A7qo)和 [Missing Semester](https://missing.csail.mit.edu/2020/course-shell/)（当然这里不再区分 Shell/Terminal/CLI）
